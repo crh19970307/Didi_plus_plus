0f7ffad8b487520d3a737550e38182a2ee0c1d8c
crh19970307 <995494637@qq.com>
2016-12-10 23:24:29 +0800

    Add files via upload

---
+++ query.py
@@ -0,0 +1,9 @@
+from peewee import SqliteDatabase, Model, CharField
+from app import Record, db
+import json
+
+print('Please input the ID which you want to query:')
+id = int(input())
+data = json.loads(Record.select().where(Record.id == str(id)).get().data)
+for id, key in data.items():
+	print(id + ', ' + key)
